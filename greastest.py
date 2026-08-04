73f2a66b9a2d3673f148905ab33eaa491467a56a
RAVALIPOTTI <rudrapuravali@gmail.com>
2026-08-04 13:48:21 +0530

    largest

---
+++ greastest.py
@@ -0,0 +1,9 @@
+
+num1=int(input("enter the number:"))
+num2=int(input("enter the number:"))
+if num1>num2:
+    print("largest number is :",num1)
+elif num2>num1:
+    print("largest number is :",num2)
+else:
+    print("both are equal")
